fc836af140b7be51112f95525aa5cd4fbbb5e4ab
MAQUO22 <samprise228@gmail.com>
2023-09-23 15:01:27 +0300

    added vertex and fragment shaders

--- PositronEngineCore/src/PositronEngineCore/Window.cpp
+++ PositronEngineCore/src/PositronEngineCore/Window.cpp
@@ -13,6 +13,39 @@ namespace PositronEngine
 
     static bool is_GLFW_initialized_ = false;
 
+    GLfloat points[] = {
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
+    };
+
+    GLfloat colors[] = {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    };
+
+    const char* vertex_shader =
+        "#version 460\n"
+        "layout(location = 0) in vec3 vertex_position;"
+        "layout(location = 1) in vec3 vertex_color;"
+        "out vec3 color;"
+        "void main() {"
+        "   color = vertex_color;"
+        "   gl_Position = vec4(vertex_position, 1.0);"
+        "}";
+
+    const char* fragment_shader =
+        "#version 460\n"
+        "in vec3 color;"
+        "out vec4 frag_color;"
+        "void main() {"
+        "   frag_color = vec4(color, 1.0);"
+        "}";
+
+    GLuint shader_program;
+    GLuint vertext_array_object;
+
     Window::Window(std::string title, unsigned int width, unsigned int height) :
         _window_data({std::move(title), width, height})
     {
@@ -36,6 +69,12 @@ namespace PositronEngine
 
         glClearColor(_background_color[0],_background_color[1],_background_color[2],_background_color[3]);
         glClear(GL_COLOR_BUFFER_BIT); // <--- need GL
+
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vertext_array_object);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         ImGuiIO& input_output = ImGui::GetIO();
         input_output.DisplaySize.x = static_cast<float>(getWidth());
@@ -121,6 +160,54 @@ namespace PositronEngine
                 window_data._event_callback_function(event);
             }
         );
+
+        glfwSetFramebufferSizeCallback(_window,
+            [](GLFWwindow* window,int width, int height)
+            {
+                glViewport(0, 0, width, height);
+            }
+        );
+
+
+
+
+        GLuint vertex_shader_identificator = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex_shader_identificator, 1, &vertex_shader, nullptr);
+        glCompileShader(vertex_shader_identificator);
+
+        GLuint fragment_shader_identificator = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment_shader_identificator, 1, &fragment_shader, nullptr);
+        glCompileShader(fragment_shader_identificator);
+
+        shader_program = glCreateProgram();
+        glAttachShader(shader_program, vertex_shader_identificator);
+        glAttachShader(shader_program, fragment_shader_identificator);
+
+        glLinkProgram(shader_program);
+
+        glDeleteShader(vertex_shader_identificator);
+        glDeleteShader(fragment_shader_identificator);
+
+        GLuint points_vbo = 0;
+        glGenBuffers(1, &points_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+        GLuint colors_vbo = 0;
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+        glGenVertexArrays(1, &vertext_array_object);
+        glBindVertexArray(vertext_array_object);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
         return 0;
     }
